b1d719b0f2c67fc954481f827bca451c6d727894
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2024-08-09 13:45:57 +0800

    Update shootercommand.java

--- src/main/java/frc/robot/commands/shootercommand.java
+++ src/main/java/frc/robot/commands/shootercommand.java
@@ -7,30 +7,34 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class shootercommand extends Command {
     private shooter shooter;
-    private double Voltage;
+    private double VoltageL;
     private intaker intaker;
+    private double VoltageH;
+    private boolean flag;
 
-    public shootercommand(shooter shooter, intaker intaker, double Voltage) {
+    public shootercommand(shooter shooter, intaker intaker, double VoltageL , double VoltageH) {
         this.shooter = shooter;
-        this.Voltage = Voltage;
+        this.VoltageL = VoltageL;
         this.intaker = intaker;
+        this.VoltageH = VoltageH;
         addRequirements(shooter, intaker);
     }
 
     @Override
     public void execute() {
-        shooter.setshooter(Voltage);
+        shooter.setshooter(VoltageL,VoltageH);
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         intaker.setintaker(1.0);
+        flag = false;
     }
 
     @Override
     public void end(boolean interrupted) {
-        shooter.setshooter(0);
+        shooter.setshooter(0,0);
         intaker.setintaker(0);
     }
 }
