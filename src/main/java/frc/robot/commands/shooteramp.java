71788a5204c165a4e2c2bf4751f5963e84439e76
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2024-08-09 13:45:31 +0800

    Update shooteramp.java

--- src/main/java/frc/robot/commands/shooteramp.java
+++ src/main/java/frc/robot/commands/shooteramp.java
@@ -9,13 +9,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 /** An example command that uses an example subsystem. */
 public class shooteramp extends Command {
     private shooter shooter;
-    private double Voltage;
+    private double VoltageL;
+    private double VoltageH;
     private intaker intaker;
     // private BeamBreak intakerBeamBreak = new BeamBreak(3);
 
-    public shooteramp(shooter shooter, intaker intaker, double Voltage) {
+    public shooteramp(shooter shooter, intaker intaker, double VoltageL, double VoltageH) {
         this.shooter = shooter;
-        this.Voltage = Voltage;
+        this.VoltageL = VoltageL;
+        this.VoltageH = VoltageH;
         this.intaker = intaker;
         addRequirements(shooter, intaker);
     }
@@ -23,12 +25,13 @@ public class shooteramp extends Command {
     @Override
     public void execute() {
         intaker.setintaker(1.0);
-        shooter.setshooter(Voltage);
+        shooter.setshooter(VoltageL , VoltageH);
     }
 
     @Override
     public void end(boolean interrupted) {
-        shooter.setshooter(0);
+        shooter.setshooter(0,0);
         intaker.setintaker(0);
     }
 }
+
