504e12f3674eab3af707ec3b78a80d16afeb18aa
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2024-08-09 13:45:04 +0800

    Update intakerout.java

--- src/main/java/frc/robot/commands/intakerout.java
+++ src/main/java/frc/robot/commands/intakerout.java
@@ -1,8 +1,12 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.intaker.*;
+import frc.robot.subsystems.shooter.*;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
+/** An example command that uses an example subsystem. */
 public class intakerout extends Command {
     private final intaker intaker;
     private final shooter shooter;
@@ -16,12 +20,14 @@ public class intakerout extends Command {
     @Override
     public void execute() {
         intaker.setintaker(-0.7);
-        shooter.setshooter(0);
+        shooter.setshooter(0 , 0);
     }
 
     @Override
     public void end(boolean interrupted) {
         intaker.setintaker(0);
     }
+
+    
 
 }
