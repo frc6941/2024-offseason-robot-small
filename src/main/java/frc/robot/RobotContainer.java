de075b1b49d518a26ec48b20179e8618a3ef3408
hs150521 <hs150521@163.com>
2024-08-15 20:37:45 +0800

    feat: auto selector

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,17 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.display.Display;
 import frc.robot.drivers.BeamBreak;
 import frc.robot.subsystems.limelight.Limelight;
 import frc.robot.utils.AllianceFlipUtil;
+import frc.robot.utils.Utils;
 import org.frcteam6941.looper.UpdateManager;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -28,6 +34,7 @@ import frc.robot.subsystems.intaker.intaker;
 import frc.robot.subsystems.shooter.shooter;
 import frc.robot.subsystems.led.led;
 import lombok.Getter;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class RobotContainer {
     private BeamBreak intakerBeamBreakH = new BeamBreak(3);
@@ -40,6 +47,8 @@ public class RobotContainer {
     led led = new led();
     Limelight limelight = Limelight.getInstance();
     Display display = Display.getInstance();
+    @Getter
+    private LoggedDashboardChooser<Command> autoChooser;
 
     @Getter
     private UpdateManager updateManager;
@@ -50,10 +59,46 @@ public class RobotContainer {
                 display);
         updateManager.registerAll();
 
+        configureAuto();
         configureBindings();
         System.out.println("Init Completed!");
     }
 
+    /** Bind Auto */
+    private void configureAuto() {
+        //TODO: register commands
+//        NamedCommands.registerCommand("AutoShoot", speakerAutoShot().withTimeout(2.0));
+        AutoBuilder.configureHolonomic(
+                () -> Swerve.getInstance().getLocalizer().getCoarseFieldPose(0),
+                (Pose2d pose2d) -> Swerve.getInstance().resetPose(pose2d),
+                () -> Swerve.getInstance().getChassisSpeeds(),
+                (ChassisSpeeds chassisSpeeds) -> Swerve.getInstance().driveSpeed(chassisSpeeds),
+                new HolonomicPathFollowerConfig(
+                        Constants.SwerveConstants.maxSpeed.magnitude(),
+                        0.55,
+                        new ReplanningConfig()),
+//                new HolonomicPathFollowerConfig(
+//                        new PIDConstants(
+//                                Constants.AutoConstants.swerveXGainsClass.swerveX_KP.get(),
+//                                Constants.AutoConstants.swerveXGainsClass.swerveX_KI.get(),
+//                                Constants.AutoConstants.swerveXGainsClass.swerveX_KD.get()
+//                        ),
+//                        new PIDConstants(
+//                                Constants.AutoConstants.swerveOmegaGainsClass.swerveOmega_KP.get(),
+//                                Constants.AutoConstants.swerveOmegaGainsClass.swerveOmega_KI.get(),
+//                                Constants.AutoConstants.swerveOmegaGainsClass.swerveOmega_KD.get()
+//                        ),
+//                        Constants.SwerveConstants.maxSpeed.magnitude(),
+//                        0.55,
+//                        new ReplanningConfig()),
+                Utils::flip,
+                swerve
+        );
+
+        autoChooser = new LoggedDashboardChooser<>("Chooser", AutoBuilder.buildAutoChooser());
+        //TODO: operator dashboard
+//        dashboard.registerAutoSelector(autoChooser.getSendableChooser());
+    }
     /** Bind controller keys to commands */
     private void configureBindings() {
         // Drive mode 1
