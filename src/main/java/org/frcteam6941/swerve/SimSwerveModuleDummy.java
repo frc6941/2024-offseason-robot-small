f5a3e998f45173ecc2aeac3db113d89aa97f1608
Mai961 <michael96mai@gmail.com>
2024-10-30 12:24:11 +0800

    Fix sim flip

--- src/main/java/org/frcteam6941/swerve/SimSwerveModuleDummy.java
+++ src/main/java/org/frcteam6941/swerve/SimSwerveModuleDummy.java
@@ -29,8 +29,8 @@ public class SimSwerveModuleDummy implements SwerveModuleBase {
     @Override
     public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop, boolean overrideMotion) {
         currentState = desiredState;
-        int flipCoefficient = Utils.flip() ? -1 : 1;
-        currentPosition.distanceMeters += flipCoefficient * desiredState.speedMetersPerSecond * Constants.LOOPER_DT * 0.95;
+        //int flipCoefficient = Utils.flip() ? -1 : 1;
+        currentPosition.distanceMeters += desiredState.speedMetersPerSecond * Constants.LOOPER_DT * 0.95;
         currentPosition.angle = desiredState.angle;
     }
 
